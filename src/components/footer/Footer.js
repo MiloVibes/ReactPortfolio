fa703dc3f953efb7eb498d175ba7b28a9ace8dbe
MiloVibes <73610862+MiloVibes@users.noreply.github.com>
2023-04-19 00:03:19 -0500

    footer changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,8 +14,8 @@ export default function Footer() {
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+          <a href="https://github.com/MiloVibes/ReactPortfolio.git">
+          ReactPortfolio
           </a>
         </p>
       </div>
