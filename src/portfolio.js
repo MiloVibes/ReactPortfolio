73ae90331152558373f6b3e159406e39ed0a8420
MiloVibes <73610862+MiloVibes@users.noreply.github.com>
2023-04-18 23:47:20 -0500

    new twitter

--- src/portfolio.js
+++ src/portfolio.js
@@ -370,7 +370,7 @@ const contactInfo = {
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "shanselman", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
